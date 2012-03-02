a8758975d0c1b9123a261ecf0d5c9c8c9c89e413
Ben Augarten <baugarten@berkeley.edu>
2012-03-02 13:25:24 -0800

    Removing some stuff

--- gen/com/owleyes/moustache/R.java
+++ gen/com/owleyes/moustache/R.java
@@ -10,44 +10,82 @@ package com.owleyes.moustache;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int five=0x7f040005;
+        public static final int four=0x7f040004;
+        public static final int one=0x7f040001;
+        public static final int three=0x7f040003;
+        public static final int two=0x7f040002;
+        public static final int zero=0x7f040000;
+    }
     public static final class drawable {
-        public static final int clk=0x7f020000;
-        public static final int ctr_clk=0x7f020001;
-        public static final int eight=0x7f020002;
-        public static final int eleven=0x7f020003;
-        public static final int five=0x7f020004;
-        public static final int four=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int left=0x7f020007;
-        public static final int minus=0x7f020008;
-        public static final int moustaches=0x7f020009;
-        public static final int nine=0x7f02000a;
-        public static final int one=0x7f02000b;
-        public static final int plus=0x7f02000c;
-        public static final int right=0x7f02000d;
-        public static final int seven=0x7f02000e;
-        public static final int six=0x7f02000f;
-        public static final int ten=0x7f020010;
-        public static final int three=0x7f020011;
-        public static final int twelve=0x7f020012;
-        public static final int two=0x7f020013;
+        public static final int button=0x7f020000;
+        public static final int button_pressed=0x7f020001;
+        public static final int clk=0x7f020002;
+        public static final int clk2=0x7f020003;
+        public static final int clk_pressed=0x7f020004;
+        public static final int ctr_clk=0x7f020005;
+        public static final int ctr_clk2=0x7f020006;
+        public static final int ctr_clk_pressed=0x7f020007;
+        public static final int eight=0x7f020008;
+        public static final int eleven=0x7f020009;
+        public static final int five=0x7f02000a;
+        public static final int four=0x7f02000b;
+        public static final int icon=0x7f02000c;
+        public static final int left=0x7f02000d;
+        public static final int main_screen=0x7f02000e;
+        public static final int main_screen2=0x7f02000f;
+        public static final int main_screen3=0x7f020010;
+        public static final int minus=0x7f020011;
+        public static final int minus2=0x7f020012;
+        public static final int minus_pressed=0x7f020013;
+        public static final int moustaches=0x7f020014;
+        public static final int nine=0x7f020015;
+        public static final int one=0x7f020016;
+        public static final int plus=0x7f020017;
+        public static final int plus2=0x7f020018;
+        public static final int plus_pressed=0x7f020019;
+        public static final int remove=0x7f02001a;
+        public static final int right=0x7f02001b;
+        public static final int rotate=0x7f02001c;
+        public static final int rotate_switch=0x7f02001d;
+        public static final int save=0x7f02001e;
+        public static final int scale=0x7f02001f;
+        public static final int scale_switch=0x7f020020;
+        public static final int seven=0x7f020021;
+        public static final int six=0x7f020022;
+        public static final int temp=0x7f020023;
+        public static final int ten=0x7f020024;
+        public static final int three=0x7f020025;
+        public static final int trash1=0x7f020026;
+        public static final int trash2=0x7f020027;
+        public static final int trash_closed=0x7f020028;
+        public static final int twelve=0x7f020029;
+        public static final int two=0x7f02002a;
     }
     public static final class id {
-        public static final int ignore_warning=0x7f050005;
-        public static final int image=0x7f050001;
-        public static final int moustache=0x7f050002;
-        public static final int root=0x7f050003;
-        public static final int root_layout=0x7f050000;
-        public static final int warning_text=0x7f050004;
+        public static final int ignore_warning=0x7f070006;
+        public static final int image=0x7f070001;
+        public static final int main_screen=0x7f070002;
+        public static final int moustache=0x7f070003;
+        public static final int root=0x7f070004;
+        public static final int root_layout=0x7f070000;
+        public static final int share=0x7f070008;
+        public static final int switch_mode=0x7f070007;
+        public static final int warning_text=0x7f070005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int moustachelist=0x7f030001;
-        public static final int nothing=0x7f030002;
-        public static final int save_location_warning=0x7f030003;
+        public static final int main_screen=0x7f030001;
+        public static final int moustachelist=0x7f030002;
+        public static final int nothing=0x7f030003;
+        public static final int save_location_warning=0x7f030004;
+    }
+    public static final class menu {
+        public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
